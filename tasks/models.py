b8095d65fc9fd39bfba7f00dedcb6d969722c5b3
Mariana Lishchynska <lishchynska.m@gmail.com>
2025-02-03 21:22:00 +0200

    Initialize models for app tasks

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,59 @@
+from task_manager import settings
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Position(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(Position, on_delete=models.SET_NULL, null=True, blank=True)
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    members = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="teams")
+
+    def __str__(self):
+        return self.name
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name="projects")
+
+    def __str__(self):
+        return self.name
+
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    class PriorityChoices(models.TextChoices):
+        URGENT = 'URGENT', "Urgent"
+        HIGH = 'HIGH', "High"
+        MEDIUM = 'MEDIUM', "Medium"
+        LOW = 'LOW', "Low"
+        NONE = 'NONE', "No Priority"
+
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    deadline = models.DateTimeField(null=True, blank=True)
+    is_completed = models.BooleanField(default=False)
+    priority = models.CharField(max_length=10, choices=PriorityChoices, default=PriorityChoices.NONE)
+    task_type = models.ForeignKey(TaskType, on_delete=models.SET_NULL, null=True, blank=True)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name="tasks", null=True, blank=True)
+    assignees = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="tasks", blank=True)
+
+    def __str__(self):
+        return self.name
