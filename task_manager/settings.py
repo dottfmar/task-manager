117c008fa342113e7ec600e03c235a3770b1b509
Mariana Lishchynska <lishchynska.m@gmail.com>
2025-02-05 13:20:07 +0200

    Adjust settings

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+from decouple import config
 from django.conf.global_settings import STATICFILES_DIRS
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mr&4-3kub5mrenm5v#g@-js5+fye^#xn^^a3v^o2wmo*0p9^er'
+SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
